9dc87ec7e9ef7f68b5fe8404d9cc404d213a4486
MontenegroAlessandro <alessandro.montenegro@mail.polimi.it>
2023-01-21 17:14:04 +0100

    Interactive experiment 1

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # Libraries
 import json
+import sys
 from environment.environment import SRBEnvironment
 from agent.agent import *
 from runner.runner import Runner
+from config.oracle import OracleUCBSrb, OracleUCBe, OracleSR
+
+horizon = int(sys.argv[1])
 
 # Arms
 f1 = lambda x: 1 - 40 / (40 + x)
@@ -10,22 +14,55 @@ f2 = lambda x: .88 * (1 - 10 / (10 + x))
 f3 = lambda x: .7 * (1 - 10 / (10 + x))
 f4 = lambda x: .5 * (1 - 20 / (20 + x))
 arms = [f1, f2, f3, f4]
+convergence_points = [1, .88, .7, .5]
+eps = .25
+sigma = 0.01
+
+oracle_agent_ucb_e = OracleUCBe(arms=arms, horizon=horizon)
+oracle_ucb_srb = OracleUCBSrb(arms=arms, convergence_points=convergence_points, eps=eps, horizon=horizon, sigma=sigma,
+                              beta=2)
+oracle_sr_srb = OracleSR(arms=arms, convergence_points=convergence_points, eps=eps, horizon=horizon, sigma=sigma,
+                              beta=2)
 
 # load parameters
 config_name = "exp_1"
 with open('config/' + config_name + '.json') as json_file:
     param = json.load(json_file)
+    param["horizon"] = horizon
+
     param_agent_unif = deepcopy(param['agent_unif'])
+    param_agent_unif["horizon"] = horizon
+
     param_agent_unif_smooth = deepcopy(param['agent_unif_smooth'])
+    param_agent_unif_smooth["horizon"] = horizon
+
     param_agent_ucb = deepcopy(param['agent_ucb'])
+    param_agent_ucb["horizon"] = horizon
+    param_agent_ucb["exp_param"] = oracle_agent_ucb_e.optimal_a
+
     param_agent_ucb_srb = deepcopy(param['agent_ucb_srb'])
+    param_agent_ucb_srb["horizon"] = horizon
+    param_agent_ucb_srb["exp_param"] = oracle_ucb_srb.optimal_a
+
     param_agent_sr = deepcopy(param['agent_sr'])
+    param_agent_sr["horizon"] = horizon
+
     param_agent_sr_srb = deepcopy(param['agent_sr_srb'])
+    param_agent_sr_srb["horizon"] = horizon
+
     param_agent_prob = deepcopy(param['agent_prob'])
+    param_agent_prob["horizon"] = horizon
+
     param_agent_etc = deepcopy(param['agent_etc'])
+    param_agent_etc["horizon"] = horizon
+
     param_agent_rest_sure = deepcopy(param['agent_rest_sure'])
+    param_agent_rest_sure["horizon"] = horizon
+
     param_env = deepcopy(param['env'])
     param_env['actions'] = arms
+    param_env['horizon'] = horizon
+
 
 # Build up the blocks
 env = SRBEnvironment(**param_env)
@@ -60,7 +97,7 @@ runner = Runner(
     log_path="experiments"
 )
 
-print("\n################# T = " + str(runner.horizon) + " #################")
+print("\n################# T = " + str(horizon) + " #################")
 for agent in agents:
     # assign the new agent
     runner.agent = agent
@@ -72,5 +109,6 @@ for agent in agents:
     print("Empirical Error " + str(agent.name) + ": " + str((runner.n_trials - count) / runner.n_trials))
 
 # save the output
-runner.save_output(config_name + "_horizon_" + str(runner.horizon))
+name = str(horizon)
+runner.save_output(name)
 print("##############################################################")
